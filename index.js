93f8b020ef5ffb1813510ac7ec8eb9c11af5fb9d
Malthe Petersen <Malthep1@gmail.com>
2021-08-07 08:42:48 +0200

    Fin

--- index.js
+++
@@ -1,9 +0,0 @@
-window.navigator.geolocation.getCurrentPosition(
-  function(position) {
-    console.log(`latitude: ${position.coords.latitude}`);
-    console.log(`longitude: ${position.coords.longitude}`);
-  },
-  function(error) {
-    console.log("Error or user didn't share location.");
-  }
-);
